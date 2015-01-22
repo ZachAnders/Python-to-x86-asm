40afb1f24416403f7f530a713b37fcd6029ddc44
Brian Bauer <thesourfox@gmail.com>
2015-01-22 10:45:50 -0700

    small changes

--- p0/src/operations/instrs.py
+++ p0/src/operations/instrs.py
@@ -9,3 +9,16 @@ class OpAdd(Operation):
         left = self.mem.get(self.args[0])
         right = self.mem.get(self.args[1])
         return "addl %s, %s" % (left, right)
+
+class OpUnarySub(Operation):
+    def write(self, instWriter):
+        self.mem.ensureRegister(self.args[0])
+        node = self.mem.get(self.args[0])
+        return "negl %s" % (node)
+
+class OpPrintnl(Operation):
+    def write(self, instWriter):
+        #pushl -x(%ebp)
+        #call print_int_nl
+        #addl 4, %ebp
+
