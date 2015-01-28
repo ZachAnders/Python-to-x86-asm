9fb69f3d62cf320549d4f8d99e19d3011ae4e3ca
ZachAnders <Zacharia.Anders@gmail.com>
2015-01-27 20:28:08 -0700

    Fixed stupid copy-paste indentation bug

--- python_compiler/parser/lex.py
+++ python_compiler/parser/lex.py
@@ -1,3 +1,4 @@
+# Lexer
 tokens = ('PRINT','INT','PLUS')
 t_PRINT = r'print'
 t_PLUS = r'\+'
@@ -8,8 +9,7 @@ def t_INT(t):
     except ValueError:
         print "integer value too large", t.value
         t.value = 0
-        return t
-
+    return t
 t_ignore = ' \t'
 
 def t_newline(t):
@@ -20,27 +20,29 @@ def t_error(t):
     print "Illegal character '%s'" % t.value[0]
     t.lexer.skip(1)
 
-    
 import ply.lex as lex
 lex.lex()
 
+# Parser
 from compiler.ast import Printnl, Add, Const
 precedence = (
         ('nonassoc','PRINT'),
         ('left','PLUS')
         )
+
 def p_print_statement(t):
     'statement : PRINT expression'
     t[0] = Printnl([t[2]], None)
+
 def p_plus_expression(t):
     'expression : expression PLUS expression'
     t[0] = Add((t[1], t[3]))
+    
 def p_int_expression(t):
     'expression : INT'
     t[0] = Const(t[1])
+
 def p_error(t):
     print "Syntax error at '%s'" % t.value
 import ply.yacc as yacc
-parser = yacc.yacc()
-
-print parser.parse("print 1+1")
+yacc.yacc()
