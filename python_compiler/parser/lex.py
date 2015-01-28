27249262db9d13355cff4f2e2437a1693636c88d
ZachAnders <Zacharia.Anders@gmail.com>
2015-01-27 19:38:28 -0700

    Base thing.

---
+++ python_compiler/parser/lex.py
@@ -0,0 +1,46 @@
+tokens = ('PRINT','INT','PLUS')
+t_PRINT = r'print'
+t_PLUS = r'\+'
+def t_INT(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print "integer value too large", t.value
+        t.value = 0
+        return t
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+    
+import ply.lex as lex
+lex.lex()
+
+from compiler.ast import Printnl, Add, Const
+precedence = (
+        ('nonassoc','PRINT'),
+        ('left','PLUS')
+        )
+def p_print_statement(t):
+    'statement : PRINT expression'
+    t[0] = Printnl([t[2]], None)
+def p_plus_expression(t):
+    'expression : expression PLUS expression'
+    t[0] = Add((t[1], t[3]))
+def p_int_expression(t):
+    'expression : INT'
+    t[0] = Const(t[1])
+def p_error(t):
+    print "Syntax error at '%s'" % t.value
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+print parser.parse("print 1+1")
