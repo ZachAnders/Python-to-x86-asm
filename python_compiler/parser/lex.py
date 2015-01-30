513bd0fd149148318baee33a400e4e5b461787d9
ZachAnders <Zacharia.Anders@gmail.com>
2015-01-29 17:49:29 -0700

    Updates.

--- python_compiler/parser/lex.py
+++ python_compiler/parser/lex.py
@@ -1,6 +1,13 @@
 import ply.lex as lex
 import ply.yacc as yacc
-from compiler.ast import Printnl, Add, Const
+from compiler.ast import *
+
+class Input():
+    def __init__(self, *args):
+        self.args = args
+    def __repr__(self):
+        args = " ".join([arg for arg in self.args])
+        return "Input({args})".format(args=args)
 
 # Lexer
 tokens = (
@@ -23,6 +30,13 @@ t_UMINUS = r'-'
 t_EQUALS = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+
+def t_NAME(t):
+    '[a-zA-Z][a-zA-Z]*'
+    return t
+
+#t_NAME = r'[a-zA-Z]+'
+#t_NAME = r'a.*'
 
 def t_INT(t):
     r'\d+'
@@ -58,7 +72,7 @@ def p_print_statement(t):
     t[0] = Printnl([t[2]], None)
 
 def p_Assign_statement(t):
-    'statement : expression EQUALS expression'
+    'statement : NAME EQUALS expression'
     t[0] = Assign(t[1], t[3])
 
 def p_discard_statement(t):
@@ -70,13 +84,13 @@ def p_plus_expression(t):
     'expression : expression PLUS expression'
     t[0] = Add((t[1], t[3]))
     
+def p_name_expression(t):
+    'expression : NAME'
+    t[0] = Name(t[1])
+
 def p_int_expression(t):
     'expression : INT'
     t[0] = Const(t[1])
-
-def p_var_expression(t):
-    'expression : NAME'
-    t[0] = Var(t[1])
 
 def p_group_expression(t):
     'expression : LPAREN expression RPAREN'
@@ -88,9 +102,10 @@ def p_input_expression(t):
 
 def p_unarysub_expression(t):
     'expression : UMINUS expression'
-    t[0] = UnarySub(t[1])
+    t[0] = UnarySub(t[2])
 
 def p_error(t):
     print "Syntax error at '%s'" % t.value
 
 yacc.yacc()
+print yacc.parse("print abc")
